bdd19a52baf82e5ef22f34ea31f5e1ac2302c345
Manuel Mosquera <mosquera.manuel2011@gmail.com>
2022-04-27 18:00:54 -0500

    :wrench: Add sass to the project

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-const App: FC = () => {
-  return <h1>Hola!!!</h1>;
-};
-
-export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import styles from "./index.styles.module.scss";
+
+const App: FC = () => {
+  return <h1 className={styles.title}>Hola!!!</h1>;
+};
+
+export default App;
